5790d84363356836c4ad9c79d8a19fc58f810b4a
Mizuki Akiyama <mizuki.akiyama.app@gmail.com>
2019-10-15 23:54:30 +0900

    Updates

---
+++ precache-manifest.fc5265bc23e9f0486dcfd633482901ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c355ef52a7a56769681b0a8e6423f1c",
+    "url": "/terraforming_mars/index.html"
+  },
+  {
+    "revision": "fdfa74e1796543cfd855",
+    "url": "/terraforming_mars/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "0046b051a9c357cfce9f",
+    "url": "/terraforming_mars/static/js/2.c0dd0650.chunk.js"
+  },
+  {
+    "revision": "fdfa74e1796543cfd855",
+    "url": "/terraforming_mars/static/js/main.ce316416.chunk.js"
+  },
+  {
+    "revision": "39f20c8e9847e8670e36",
+    "url": "/terraforming_mars/static/js/runtime-main.8967dc4a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/terraforming_mars/static/media/logo.25bf045c.svg"
+  }
+]);
